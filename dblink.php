11aa093cf5b6f885710a363e1d9bac3052e5ab16
adam010 <adam010@users.noreply.github.com>
2017-04-06 18:41:45 +0200

    Delete dblink.php

--- dblink.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$mysqli = new mysqli('localhost', 'homestead', 'secret','mpmdb','33060');
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-;?>
